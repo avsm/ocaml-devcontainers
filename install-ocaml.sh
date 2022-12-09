c38e561ec79e73824d38d0ab70a28b213b30446d
Anil Madhavapeddy <anil@recoil.org>
2022-12-09 13:36:09 +0000

    also build trunk ocaml

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -4,14 +4,19 @@ set -e
 # TODO: switch to patricoferris/opam-sysinstall when it supports OCaml 5.0
 cd /root
 OCAML_VERSION=$1
+MODE=tags
 case ${OCAML_VERSION} in
+5.1.0)
+  OCAML_VERSION=trunk
+  MODE=heads
+  ;;
 5.0.0)
   OCAML_VERSION=5.0.0-rc1
   ;;
 *)
   ;;
 esac
-curl -OL https://github.com/ocaml/ocaml/archive/refs/tags/${OCAML_VERSION}.tar.gz
+curl -OL https://github.com/ocaml/ocaml/archive/refs/${MODE}/${OCAML_VERSION}.tar.gz
 tar -xf ${OCAML_VERSION}.tar.gz
 cd ocaml-${OCAML_VERSION}
 ./configure --prefix=/usr
