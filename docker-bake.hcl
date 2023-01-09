e975603eb7ddc234b9ce722e9c8adcc302d7ddf7
Anil Madhavapeddy <anil@recoil.org>
2023-01-08 20:58:19 -0800

    update versions

--- docker-bake.hcl
+++ docker-bake.hcl
@@ -1,10 +1,10 @@
 group "default" {
     targets = ["repo"]
 }
-target "ocaml_debs_4_14_0" {
+target "ocaml_debs_4_14_1" {
   dockerfile = "Dockerfile.deb"
   platforms = [ "linux/arm64", "linux/amd64" ]
-  args = { OCAML_VERSION="4.14.0" }
+  args = { OCAML_VERSION="4.14.1" }
 }
 target "ocaml_debs_5_0_0" {
   dockerfile = "Dockerfile.deb"
@@ -19,7 +19,7 @@ target "ocaml_debs_5_1_0" {
 target "repo" {
   dockerfile = "Dockerfile.all"
   contexts = {
-    debs_4_14_0 = "target:ocaml_debs_4_14_0"
+    debs_4_14_1 = "target:ocaml_debs_4_14_1"
     debs_5_0_0 = "target:ocaml_debs_5_0_0"
     debs_5_1_0 = "target:ocaml_debs_5_1_0"
   }
--- install-tools.sh
+++ install-tools.sh
@@ -8,17 +8,21 @@ OCAML_VERSION=$1
 
 opam init -ya --disable-sandboxing -c ocaml.${OCAML_VERSION}
 case ${OCAML_VERSION} in
-5.0.0)
-  opam repo add alpha https://github.com/kit-ty-kate/opam-alpha-repository.git
-  ;;
 5.1.0)
+  TRUNK=1
   opam repo add alpha https://github.com/kit-ty-kate/opam-alpha-repository.git
   ;;
 *)
+  TRUNK=0
   ;;
 esac
 
 opam pin add -n odoc --dev
 opam pin add -n opam-monorepo --dev
-opam install --confirm-level=unsafe-yes dune ocaml-lsp-server opam-monorepo ocamlformat dune-release odoc mdx
-mv /root/.opam/ocaml.${OCAML_VERSION}/bin/{dune,ocamllsp,opam-monorepo,ocamlformat,dune-release,odoc,ocaml-mdx} /tmp/installdir/usr/bin
+if [ "$TRUNK" = "0" ]; then
+  opam install --confirm-level=unsafe-yes dune ocaml-lsp-server opam-monorepo ocamlformat dune-release odoc mdx
+  mv /root/.opam/ocaml.${OCAML_VERSION}/bin/{dune,ocamllsp,opam-monorepo,ocamlformat,dune-release,odoc,ocaml-mdx} /tmp/installdir/usr/bin
+else
+  opam install --confirm-level=unsafe-yes dune
+  mv /root/.opam/ocaml.${OCAML_VERSION}/bin/{dune} /tmp/installdir/usr/bin
+fi
