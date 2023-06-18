47e5ba426a76d8f1e429f8f88edebadd4d50fff8
Anil Madhavapeddy <anil@recoil.org>
2023-06-18 17:01:26 +0100

    no odoc on trunk builds

--- install-tools.sh
+++ install-tools.sh
@@ -23,6 +23,6 @@ if [ "$TRUNK" = "0" ]; then
   opam install --confirm-level=unsafe-yes dune ocaml-lsp-server opam-monorepo ocamlformat dune-release odoc mdx
   mv /root/.opam/ocaml.${OCAML_VERSION}/bin/{dune,ocamllsp,opam-monorepo,ocamlformat,dune-release,odoc,ocaml-mdx} /tmp/installdir/usr/bin
 else
-  opam install --confirm-level=unsafe-yes dune odoc
+  opam install --confirm-level=unsafe-yes dune
   mv /root/.opam/ocaml.${OCAML_VERSION}/bin/dune /tmp/installdir/usr/bin
 fi
